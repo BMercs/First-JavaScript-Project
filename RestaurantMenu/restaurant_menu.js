13227b6c64c6364d42834746eb3e10d83c3a9ea4
leng003 <thelenganator@gmail.com>
2024-06-17 15:58:32 -0400

    fixes error and shows example logs for debugging

--- RestaurantMenu/restaurant_menu.js
+++ RestaurantMenu/restaurant_menu.js
@@ -3,12 +3,18 @@ const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
-document.getElementById('BreakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
 //MainCourse Array
 let mainCourseItem = '';
-mainCourseMenu.forEach((uten, index) => {
-mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+    
+    //Example of how to debug your code if you run into issues
+    console.log("Current item: " + item)
+    console.log("Current index: " + index)
+    console.log(mainCourseItem)
+});
 document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
 
 //Dessert Array
